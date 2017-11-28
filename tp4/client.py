d750843d7285a3264e1e92b75168ed7acfd88fa4
Jules <Jules>
2017-11-28 12:00:56 -0500

    serveur création compte

--- tp4/client.py
+++ tp4/client.py
@@ -1,4 +1,3 @@
-from hashlib import sha256
 from socketUtil import recv_msg, send_msg
 import getpass
 import socket
@@ -6,16 +5,15 @@ import socket
 
 def main():
     number = input("Menu de connexion \n1. Se connecter\n2.Creer un compte\n")
-    if(number != "1" and number != "2"):
+    if number != "1" and number != "2":
         print("Veuillez choisir l'option 1 ou 2...")
     else:
         username = input("Entrez votre nom d'usager: ")
         password = getpass.getpass('Mot de passe : ')
-        hashedPass = sha256(password.encode().hexdigest())
         destination = ("localhost", 69420)
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect(destination)
         send_msg(s, number)
         send_msg(s, username)
-        send_msg(s, hashedPass)
+        send_msg(s, password)
         s.close()
--- tp4/serveur.py
+++ tp4/serveur.py
@@ -2,6 +2,8 @@ from hashlib import sha256
 from socketUtil import recv_msg, send_msg
 import getpass
 import socket
+import os.path
+import re
 
 
 def main():
@@ -12,4 +14,19 @@ def main():
     (s, address) = serversocket.accept()
     number = recv_msg(s)
     username = recv_msg(s)
-    hashedPass = recv_msg(s)
+    password = recv_msg(s)
+    if number == "1":
+        print("Topkekekeke\n")
+    elif number == "2":
+        configPath = str.format(username, "/{0}/config.txt")
+        if(os.path.exists(configPath)):
+            send_msg(s, "L'utilisateur existe déjà!")
+        else:
+            if(re.search(r"(?=[^a-zA-Z]*[a-zA-Z])(?=[^\d]*\d+)(^.{6,12}$)", password)):
+                hashedPass = sha256(password.encode()).hexdigest()
+                os.makedirs(os.path.dirname(configPath), exist_ok=True)
+                with open(configPath, "w") as configFile:
+                    f.write(hashedPass)
+                    send_msg(s, "Création de l'utilisateur terminée")
+            else:
+                send_msg(s, "Erreur: le mot de passe doit être composé de 6 à 12 caractères, dont au moins un chiffre et une lettre")
