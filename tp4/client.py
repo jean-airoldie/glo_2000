b8af3c484cf2ae8e6cdaaf7857d12d17352f785d
Jules <Jules>
2017-11-28 14:19:51 -0500

    creation de compte, gestion d'erreur

--- tp4/client.py
+++ tp4/client.py
@@ -4,16 +4,48 @@ import socket
 
 
 def main():
-    number = input("Menu de connexion \n1. Se connecter\n2.Creer un compte\n")
-    if number != "1" and number != "2":
-        print("Veuillez choisir l'option 1 ou 2...")
-    else:
+    s = connection()
+    try:
+        number = ""
+        while(number != "1" and number != "2"):
+            number = input("Menu de connexion \n1.Se connecter\n2.Creer un compte\n")
+            if number != "1" and number != "2":
+                print("Veuillez choisir l'option 1 ou 2...")
         username = input("Entrez votre nom d'usager: ")
-        password = getpass.getpass('Mot de passe : ')
-        destination = ("localhost", 69420)
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect(destination)
+        password = getpass.getpass("Mot de passe : ")
         send_msg(s, number)
         send_msg(s, username)
         send_msg(s, password)
-        s.close()
+        serverConfirmation = ""
+        while(serverConfirmation != "Creation de l'utilisateur terminee"):
+            serverConfirmation = recv_msg(s)
+            print(serverConfirmation)
+            if serverConfirmation != "Creation de l'utilisateur terminee":
+                username = input("Entrez votre nom d'usager: ")
+                password = getpass.getpass("Mot de passe : ")
+                send_msg(s, username)
+                send_msg(s, password)
+    except socket.error as e:
+        print("Erreur de socket: %s. Tentative de reconnection" % e)
+
+
+def connection():
+    connected = False
+    while (not connected):
+        try:
+            destination = ("localhost", 6969)
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect(destination)
+            connected = True
+        except Exception as e:
+            retry = input("Erreur de connection au serveur : %s. Reessayer la connection? (O ou N)\n" % e)
+            if str.upper(retry) == "O":
+                pass
+            elif str.upper(retry) == "N":
+                quit()
+    return s
+
+
+while 1:
+    if __name__ == '__main__':
+        main()
--- tp4/serveur.py
+++ tp4/serveur.py
@@ -7,26 +7,44 @@ import re
 
 
 def main():
-    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    serversocket.bind(("localhost", 69420))
-    serversocket.listen(5)
-    (s, address) = serversocket.accept()
-    number = recv_msg(s)
-    username = recv_msg(s)
-    password = recv_msg(s)
-    if number == "1":
-        print("Topkekekeke\n")
-    elif number == "2":
-        configPath = str.format(username, "/{0}/config.txt")
-        if(os.path.exists(configPath)):
-            send_msg(s, "L'utilisateur existe déjà!")
-        else:
-            if(re.search(r"(?=[^a-zA-Z]*[a-zA-Z])(?=[^\d]*\d+)(^.{6,12}$)", password)):
-                hashedPass = sha256(password.encode()).hexdigest()
-                os.makedirs(os.path.dirname(configPath), exist_ok=True)
-                with open(configPath, "w") as configFile:
-                    f.write(hashedPass)
-                    send_msg(s, "Création de l'utilisateur terminée")
-            else:
-                send_msg(s, "Erreur: le mot de passe doit être composé de 6 à 12 caractères, dont au moins un chiffre et une lettre")
+    try:
+        port = 6969
+        serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        serversocket.bind(("localhost", port))
+        serversocket.listen(5)
+        print("Serveur en ecoute sur le port {0}".format(str(port)))
+        (s, address) = serversocket.accept()
+        number = ""
+        while (number != "1" and number != "2"):
+            number = recv_msg(s)
+        success = False
+        while (not success):
+            username = recv_msg(s)
+            password = recv_msg(s)
+            if number == "1":
+                print("Topkekekeke\n")
+            elif number == "2":
+                configPath = os.getcwd() + "/{0}/config.txt".format(username)
+                if(os.path.exists(configPath)):
+                    send_msg(s, "L'utilisateur existe deja!")
+                else:
+                    if(re.search(r"(?=[^a-zA-Z]*[a-zA-Z])(?=[^\d]*\d+)(^.{6,12}$)", password)):
+                        hashedPass = sha256(password.encode()).hexdigest()
+                        os.makedirs(os.path.dirname(configPath), exist_ok=True)
+                        with open(configPath, "w") as configFile:
+                            configFile.write(hashedPass)
+                            send_msg(s, "Creation de l'utilisateur terminee")
+                            success = True
+                    else:
+                        send_msg(s, "Erreur: le mot de passe doit etre compose de 6 a 12 \
+                            caracteres, dont au moins un chiffre et une lettre")
+    except socket.error as msg:
+        print ("Socket Error: %s" % msg)
+    except TypeError as msg:
+        print ("(Le client s'est probablement deconnecte) Type Error: %s" % msg)
+
+
+while 1:
+    if __name__ == '__main__':
+        main()
