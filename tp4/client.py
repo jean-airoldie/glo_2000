3381892d57503c3d44ca2b481a9ac6c8cbfcf396
Jules <Jules>
2017-11-28 23:14:08 -0500

    envoi de courriel en cours

--- tp4/client.py
+++ tp4/client.py
@@ -1,32 +1,33 @@
 from socketUtil import recv_msg, send_msg
 import getpass
 import socket
-
+import re
 
 def main():
     s = connection()
     try:
         number = ""
         while(number != "1" and number != "2"):
-            number = input("Menu de connexion \n1.Se connecter\n2.Creer un compte\n")
+            number = input("Menu de connexion \n1. Se connecter\n2. Creer un compte\n")
             if number != "1" and number != "2":
-                print("Veuillez choisir l'option 1 ou 2...")
+                print("Veuillez choisir l'option 1 ou 2...\n")
         username = input("Entrez votre nom d'usager: ")
         password = getpass.getpass("Mot de passe : ")
         send_msg(s, number)
         send_msg(s, username)
         send_msg(s, password)
         serverConfirmation = ""
-        while(serverConfirmation != "Creation de l'utilisateur terminee"):
+        while(not (serverConfirmation == "Creation de l'utilisateur terminee" or serverConfirmation == "Connection reussie")):
             serverConfirmation = recv_msg(s)
-            print(serverConfirmation)
-            if serverConfirmation != "Creation de l'utilisateur terminee":
+            print(serverConfirmation + "\n")
+            if not (serverConfirmation == "Creation de l'utilisateur terminee" or serverConfirmation == "Connection reussie"):
                 username = input("Entrez votre nom d'usager: ")
                 password = getpass.getpass("Mot de passe : ")
                 send_msg(s, username)
                 send_msg(s, password)
+        menu(s)
     except socket.error as e:
-        print("Erreur de socket: %s. Tentative de reconnection" % e)
+        print("Erreur de socket: %s. Tentative de reconnection\n" % e)
 
 
 def connection():
@@ -44,6 +45,32 @@ def connection():
             elif str.upper(retry) == "N":
                 quit()
     return s
+
+
+def menu(s):
+    number = ""
+    while (number != "1" and number != "2" and number != "3"and number != "4"):
+        number = input("Menu principal\n1. Envoi de courriels\n2. Consultation de courriels\n3. Statistiques\n4. Quitter\n")
+        if(number != "1" and number != "2" and number != "3"and number != "4"):
+            print("Veuillez choisir une option valide")
+        else:
+            send_msg(number)
+            if(number == "1"):
+            serverConfirmation = ""
+            while(not serverConfirmation == "Succes de l'envoi"):
+                emailDestination = ""
+                while (not re.search(r"^[^@]+@[^@]+\.[^@]+$", emailDestination)):
+                    emailDestination = input("Entrez une adresse courriel de destination\n")
+                    if(re.search(r"^[^@]+@[^@]+\.[^@]+$", emailDestination)):
+                        subject = input("Entrez le sujet du courriel\n")
+                        message = input("Entrez le corps du message\n")
+                        send_msg(s, emailDestination)
+                        send_msg(s, subject)
+                        send_msg(s, message)
+                        serverConfirmation = recv_msg(s)
+                        print(serverConfirmation)
+                    else:
+                        print("Adresse courriel invalide\n")
 
 
 while 1:
--- tp4/serveur.py
+++ tp4/serveur.py
@@ -4,6 +4,8 @@ import getpass
 import socket
 import os.path
 import re
+import smtplib
+from email.mime.text import MIMEText
 
 
 def main():
@@ -22,10 +24,21 @@ def main():
         while (not success):
             username = recv_msg(s)
             password = recv_msg(s)
+            configPath = os.getcwd() + "/{0}/config.txt".format(username)
             if number == "1":
-                print("Topkekekeke\n")
+                if(os.path.exists(configPath)):
+                    hashedPass = sha256(password.encode()).hexdigest()
+                    with open(configPath, "r") as configFile:
+                            configPassword = configFile.readline()
+                            if configPassword == hashedPass:
+                                send_msg(s, "Connection reussie")
+                                success = True
+                                courriel(s, username)
+                            else:
+                                send_msg(s, "Nom d'utilisateur et mot de passe invalides")
+                else:
+                    send_msg(s, "L'utilisateur n'existe pas!")
             elif number == "2":
-                configPath = os.getcwd() + "/{0}/config.txt".format(username)
                 if(os.path.exists(configPath)):
                     send_msg(s, "L'utilisateur existe deja!")
                 else:
@@ -36,6 +49,7 @@ def main():
                             configFile.write(hashedPass)
                             send_msg(s, "Creation de l'utilisateur terminee")
                             success = True
+                            courriel(s, username)
                     else:
                         send_msg(s, "Erreur: le mot de passe doit etre compose de 6 a 12 \
                             caracteres, dont au moins un chiffre et une lettre")
@@ -43,6 +57,27 @@ def main():
         print ("Socket Error: %s" % msg)
     except TypeError as msg:
         print ("(Le client s'est probablement deconnecte) Type Error: %s" % msg)
+
+
+def courriel(s, username):
+    number = ""
+    while (number != "1" and number != "2" and number != "3"and number != "4"):
+        number = recv_msg(s)
+    if (number == "1"):
+        emailDestination = ""
+        while (not re.search(r"^[^@]+@[^@]+\.[^@]+$", emailDestination)):
+            emailDestination = recv_msg(s)
+        subject = recv_msg(s)
+        message = recv_msg(s)
+        msgToSend = MIMEText(message)
+        msgToSend["From"] = username
+        msgToSend["To"] = emailDestination
+        msgToSend["Subject"] = subject
+        # if(re.search(r"^[^@]+(@reseauglo.ca)$", emailDestination)):
+        #     with open('filename.elm', 'w') as out:
+        #         gen = email.generator.Generator(out)
+        #         gen.flatten(msg)
+
 
 
 while 1:
